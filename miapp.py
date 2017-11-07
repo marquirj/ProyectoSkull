8a0feef052d909e677cea699a1c75961983a8108
marquirj <juanmartinquiros@correo.ugr.es>
2017-11-08 00:50:14 +0100

    Prueba flask

---
+++ miapp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def prueba():
+ data = {'status': 'OK'}
+ return json.dumps(data)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)

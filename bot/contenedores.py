817ec3e3f1b0f7511a91839f913f1743f90b56a7
marquirj <juanmartinquiros@correo.ugr.es>
2017-11-09 23:34:29 +0100

    ordeando lista

--- bot/contenedores.py
+++ bot/contenedores.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-equiposPuntos = {"Tabla": [ { "Chiclana C.F": 25, "Trebujena C.F": 25, "Jerez Industrial C.F.": 21, "Bazan": 20, "Balon de Cadiz": 20, "U.D. Villamartin": 17, "C.D. Vejer": 15, "Portuense C.F.": 14, "SanFer Isleño": 14, "Roteña": 12, "U.D. Tesorillo": 12, "Juventud Sanliqueña": 11, "Rayo Sanluqueño C.D.": 10, "Jedula C.D.": 8, "C.D. Guadiaro": 6, "Chipiona": 3, "El Torno 2009": 6, "San Jose Obrero U.D.": 5}
-]}
+equiposPuntos = [("Chiclana C.F", 25), ("Trebujena C.F", 25), ("Jerez Industrial C.F.", 21), ("Bazan", 20), ("Balon de Cadiz", 20), ("U.D. Villamartin", 17), ("C.D. Vejer", 15), ("Portuense C.F.", 14), ("SanFer Isleño", 14), ("Roteña", 12), ("U.D. Tesorillo", 12), ("Juventud Sanluqueña", 11), ("Rayo Sanluqueño C.D.", 10), ("Jedula C.D.", 8), ("C.D. Guadiaro", 6), ("Chipiona", 3), ("El Torno 2009", 6), ("San Jose Obrero U.D.", 5)]
 
 def getClasificacion():
-	return equiposPuntos
+ equiposPuntos.sort(key=lambda puntos: puntos[1], reverse=True)
+ return equiposPuntos
+#futbolistasTup.sort(key=lambda futbolista: futbolista[0])

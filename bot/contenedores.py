feaefee5a31672a2733d847e51b5d4da9df3251a
marquirj <juanmartinquiros@correo.ugr.es>
2017-11-09 22:55:17 +0100

    hola

--- bot/contenedores.py
+++ bot/contenedores.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 equiposPuntos = {"Tabla": [ { "Chiclana C.F": 25, "Trebujena C.F": 25, "Jerez Industrial C.F.": 21, "Bazan": 20, "Balon de Cadiz": 20, "U.D. Villamartin": 17, "C.D. Vejer": 15, "Portuense C.F.": 14, "SanFer Isleño": 14, "Roteña": 12, "U.D. Tesorillo": 12, "Juventud Sanliqueña": 11, "Rayo Sanluqueño C.D.": 10, "Jedula C.D.": 8, "C.D. Guadiaro": 6, "Chipiona": 3, "El Torno 2009": 6, "San Jose Obrero U.D.": 5}
 ]}
 

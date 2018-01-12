9d8ccf7c78354896c27338bb04b98d9cf7f5da2a
marquirj <juanmartinquiros@correo.ugr.es>
2018-01-12 18:47:12 +0100

    playbook.yml

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from fabric.api import sudo, cd, env, run, shell_env
+import os
+
+
+def instalacion():
+    # Descargamos el repositorio
+    sudo('git clone https://github.com/marquirj/ProyectoSkull.git')
+    sudo('pip install -r ~/ProyectoSkull/requirements.txt')
+
+def borrar():
+    # Borramos directorio repo
+    sudo(' rm -rf ./ProyectoSkull')
+
+def levantar():
+     # Iniciamos el servicio web
+     sudo('nohup python3 /home/black-wildflower-99/ProyectoSkull/bot/app.py',pty=False)
